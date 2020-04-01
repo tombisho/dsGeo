ff91ca5a3311aa3f0c416485540679c63437442d
Tom Bishop <trpb2@cantab.net>
2020-04-01 13:51:14 +0100

    make it work

---
+++ R/nothingDS.R
@@ -0,0 +1,12 @@
+#'
+#' @title Does nothing
+#' @description This function does nothing
+#' @return a message
+#' @author Bishop, T.
+#' @export
+#'
+
+nothingDS <- function(){
+  my_ans = "answer"
+    return(my_ans)
+}
